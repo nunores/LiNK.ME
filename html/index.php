9f81ae3f7c56b76654f0d621c7bd773e176c5b22
Xavier-Pisco <up201806134@fe.up.pt>
2021-02-28 15:51:18 +0000

    Initial commit with template-piu

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+  </head>
+  <body>
+    Content from html...
+  <?php
+    echo 'content from php';
+  ?>
+  </body>
+</html>
