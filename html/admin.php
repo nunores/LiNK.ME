18dea87802fb08fc39a36f7ed971691f139a53b4
Resende <n.resende2000@gmail.com>
2021-03-10 16:10:57 +0000

    Added admin modifications
    Created new files to accomodate admin page's requirements, responsive design is included,

---
+++ html/admin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
+	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="./css/mobile.css" />
+	<link rel="stylesheet" href="./css/post.css" />
+	<link rel="stylesheet" href="./css/left_col_admin.css" />
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+
+	<title>LiNK.ME</title>
+</head>
+ 
+<body>
+	<header>
+		<?php
+		include('./templates/navbar_admin.php');
+		?>
+	</header>
+
+	<div class="container-fluid">
+		<div class="row">
+			<?php
+			include('./templates/left_col_admin.php');
+			?>
+			<div class="col-8">
+				<div id="center-col">
+					<?php
+					include('./templates/post.php');
+					include('./templates/post.php');
+					?>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</body>
+
+</html>
