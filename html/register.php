03940a866f86a720a812bc34a8acad7d60508def
Resende <n.resende2000@gmail.com>
2021-03-11 17:02:48 +0000

    Added links
    Added links to the register and login interactions for better demonstration

--- html/register.php
+++ html/register.php
@@ -2,31 +2,31 @@
 <html>
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content=" width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1">
 
-	<!-- Bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
-	<link rel="stylesheet" href="style.css" />
-	<link rel="stylesheet" href="./css/mobile.css" />
-	<link rel="stylesheet" href="./css/post.css" />
-	<link rel="stylesheet" href="./css/left_col.css" />
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="./css/mobile.css" />
+    <link rel="stylesheet" href="./css/post.css" />
+    <link rel="stylesheet" href="./css/left_col.css" />
     <link rel="stylesheet" href="./css/login.css" />
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 
-	<title>LiNK.ME</title>
+    <title>LiNK.ME</title>
 </head>
 
 <body>
-	<header>
-		<?php
-		include('./templates/navbar.php');
-		?>
-	</header>
+    <header>
+        <?php
+        include('./templates/navbar.php');
+        ?>
+    </header>
 
-	<div class="container-fluid">
-		<div class="row">
+    <div class="container-fluid">
+        <div class="row">
             <div class="col-2 text-center" id="left-col">
                 <div id="credentials-input">
                     <form>
@@ -37,12 +37,15 @@
                     </form>
                 </div>
                 <div id="login-button" class="card bg-dark border-secondary container-fluid">
-                    <div class="card-body">
-                        <p class="card-text">Register</p>
-                    </div>
+
+                    <a href="index.php">
+                        <div class="card-body">
+                            <p class="card-text">Register</p>
+                        </div>
+                    </a>
                 </div>
                 <div id="login-messages">
-                    <p>Already have an account?</p>
+                    <p> <a href="login.php"> Already have an account? </a></p>
                 </div>
                 <div class="feed-change">
                     <div id="feed-order">
@@ -66,16 +69,16 @@
                     <a href="#" class="link-light">FAQ</a>
                 </div>
             </div>
-			<div class="col-8">
-				<div id="center-col">
-					<?php
-					include('./templates/post.php');
-					include('./templates/post.php');
-					?>
-				</div>
-			</div>
-		</div>
-	</div>
+            <div class="col-8">
+                <div id="center-col">
+                    <?php
+                    include('./templates/post.php');
+                    include('./templates/post.php');
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
