4ef382c70bd2823a8043d5ea9430fbe34a659c78
Mine4Phantom <coelho.pedromiguel@gmail.com>
2021-03-11 17:18:15 +0000

    Added button for mobile

--- html/group_page.php
+++ html/group_page.php
@@ -135,25 +135,20 @@
             </div>
 			<div class="col-8">
 				<div id="center-col">
-                    <div class="post">
+                    <div class="invite-card">
                         <div class="container-fluid">
                             <div class="row">
-                                <div class="post-header">
-                                    <div class="col-1">
-                                        <a href="#">
-                                            <img src="./images/jlopes.png" class="rounded-circle post-profile-pic" alt="Profile picture">
-                                        </a>
-                                    </div>
-                                    <div class="post-name col-10">
-                                        <a href="#">
-                                            <span id="name-tag"> @JLopes </span>
-                                            <span id="person-name"> João Correia Lopes </span>
-                                        </a>
-                                    </div>
-                                    <div class="col-1 three-dots">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                                        </svg>
+                                <p>Add people to your group</p>
+                            </div>
+                            <div class="card bg-dark border-secondary container-fluid">
+                                <div class="row">
+                                    <div class="card-body col-8 invite-button">
+                                        <div class="add-person">
+                                            Invite
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16" color="#1AB2A8">
+                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                            </svg>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
