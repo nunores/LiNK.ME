be2e1da2af2a908887deddbde1c3e022e57d1ba7
João Gonçalves <50209733+Ranhocas@users.noreply.github.com>
2021-04-23 11:58:53 +0100

    Laravel changes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,7 +12,7 @@
 */
 // Home
 Route::get('/', 'Auth\LoginController@home');
-
+/*
 // Cards
 Route::get('cards', 'CardController@list');
 Route::get('cards/{id}', 'CardController@show');
@@ -23,7 +23,7 @@ Route::delete('api/cards/{card_id}', 'CardController@delete');
 Route::put('api/cards/{card_id}/', 'ItemController@create');
 Route::post('api/item/{id}', 'ItemController@update');
 Route::delete('api/item/{id}', 'ItemController@delete');
-
+*/
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
