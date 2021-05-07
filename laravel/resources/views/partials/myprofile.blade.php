86961604db40cb9f55b2def7437f53d76d20418c
Mine4Phantom <coelho.pedromiguel@gmail.com>
2021-05-07 22:10:02 +0100

    Added profile friends scrollable

--- laravel/resources/views/partials/myprofile.blade.php
+++ laravel/resources/views/partials/myprofile.blade.php
@@ -17,7 +17,6 @@
                     </div>
                 </div>
             </div>
-            <!--
             <div id="notifications">
                 <div id="notifications-title">
                     <p class="text-center">Notifications</p>
@@ -25,193 +24,24 @@
                         <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
                     </svg>
                 </div>
-                <div class="card bg-dark border-secondary container-fluid">
-                    <div class="row">
-                        <div class="card-body col-8">
-                            @JLopes has sent you a friend request
-                        </div>
-                        <div class="notification-friend-request col-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-                <div class="card bg-dark border-secondary container-fluid">
-                    <div class="row">
-                        <div class="card-body col-8">
-                            You have new comments in a post
-                        </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="col-4 bi bi-arrow-up-right-circle" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.854 10.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z" />
-                        </svg>
-                    </div>
-                </div>
-            </div> // TODO Put notifications here
-            -->
-            <!--
+                @php
+                    $notifications = Auth::user()->user->notifications
+                @endphp
+                @for ($i = 0; $i < 2; $i++)
+                    @if ($notifications[$i]->friendRequest == false)
+                        @include('partials.notification', ['notification' => $notifications[$i] ]) <!-- TODO Need notifications in db to test -->
+                    @endif
+                @endfor
+            </div>
             <div class="person-friends">
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/jlopes.png" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @JLopes </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/lizandroSilva.png" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Lizaaandro </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xavierMontanelas.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @XM </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/maria.png" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Maria </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xavierLopes.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @XLopes </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-                <div class="post-header person-friends-header">
-                    <div class="col-1">
-                        <a href="./other_profile.php">
-                            <img src="./images/xicoXavier.jpg" class="rounded-circle person-friends-profile-pic" alt="Profile picture">
-                        </a>
-                    </div>
-                    <div class="post-name col-6">
-                        <a href="./other_profile.php">
-                            <span id="name-tag" class="person-friends-name-tag"> @Xico </span>
-                        </a>
-                    </div>
-                </div>
-            </div> // TODO Add friends scrollable
-            -->
+                @php
+                    $links = Auth::user()->user->links;
+                @endphp
+                @for ($i = 0; $i < count($links) && $i < 10; $i++) <!-- TODO Change limit number or remove -->
+                    @include('partials.friend', ['user' => $links[$i] ])
+                @endfor
+            </div>
+
             <div id="groups">
                 @php
                     $groups = Auth::user()->user->groups;
