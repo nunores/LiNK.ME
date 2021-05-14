797dcb978204707724e12f27b06996274ddcdb54
xavier <up201806134@fe.up.pt>
2021-05-14 15:11:33 +0100

    Group search for links working

---
+++ laravel/public/js/friends_search.js
@@ -0,0 +1,20 @@
+const add_form = document.querySelector("#add-search");
+
+add_form.onsubmit = search_friends;
+
+function search_friends(event) {
+    event.preventDefault();
+    const value = this.querySelector("input[type=search]").value;
+    const headers = document.querySelectorAll(".person-friends-header");
+    console.log(value);
+    headers.forEach(header => {
+        const name_tag = header.querySelector(".person-friends-name-tag");
+        const name = name_tag.getAttribute("data-user-name");
+        const username = name_tag.innerHTML;
+        if (!(name.includes(value) || username.includes(value))) {
+            header.hidden = true;
+        } else {
+            header.hidden = false;
+        }
+    });
+}
