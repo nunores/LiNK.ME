70c80fdda70d8d7b3013edb8a856dd0ffb3b42ce
xavier <up201806134@fe.up.pt>
2021-06-03 13:01:22 +0100

    Fixed infinite scroll button

--- laravel/public/js/infinite_scrolling.js
+++ laravel/public/js/infinite_scrolling.js
@@ -7,29 +7,24 @@ function load_more_posts() {
     AJAX("GET", "/api/more_posts?page=" + next_page, {_token: _token}, function() {
         console.log(this.responseText);
         const response = JSON.parse(this.responseText);
-        if (response['data'].length == 0) {
-            load_more_button.hidden = true;
-        } else {
-            response['data'].forEach((post) => {
-                const post_id = post['id'];
-                AJAX("GET", "/api/post/" + post_id, { _token: _token }, function () {
-                    const div = document.createElement("div");
-                    div.innerHTML = this.responseText.trim();
-                    div.querySelector(".bi-arrow-right-circle").onclick = sendComment;
-                    div.querySelector(".bi-hand-thumbs-up").onclick = clickedLike;
-                    div.querySelector(".bi-hand-thumbs-down").onclick = clickedDislike;
-                    if (div.querySelector(".delete-post") != null)
-                        div.querySelector(".delete-post").onclick = delete_post;
-
-                    load_more_button.parentNode.insertBefore(div.firstChild, load_more_button);
-
-                    const form = document.querySelector("#add-post-form");
-                    form.parentNode.remove();
-                    add_post_button.hidden = false;
-                    return_button.hidden = true;
-                });
-            });
+        if (response['current_page'] >= response['last_page']) {
+            console.log('here');
+            load_more_button.style.display = "none";
         }
         load_more_button.setAttribute("data-page-id", next_page);
+        response['data'].forEach((post) => {
+            const post_id = post['id'];
+            AJAX("GET", "/api/post/" + post_id, { _token: _token }, function () {
+                const div = document.createElement("div");
+                div.innerHTML = this.responseText.trim();
+                div.querySelector(".bi-arrow-right-circle").onclick = sendComment;
+                div.querySelector(".bi-hand-thumbs-up").onclick = clickedLike;
+                div.querySelector(".bi-hand-thumbs-down").onclick = clickedDislike;
+                if (div.querySelector(".delete-post") != null)
+                    div.querySelector(".delete-post").onclick = delete_post;
+
+                load_more_button.parentNode.insertBefore(div.firstChild, load_more_button);
+            });
+        });
     });
 }
