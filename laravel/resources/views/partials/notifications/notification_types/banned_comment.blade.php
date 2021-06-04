b533cc5f356e91e3fa381aef0751a09f57eae162
João <up201806796@fe.up.pt>
2021-06-04 09:49:22 +0100

    Fixed css on admin reports and changed banned comment text to know which comment it is talking about

--- laravel/resources/views/partials/notifications/notification_types/banned_comment.blade.php
+++ laravel/resources/views/partials/notifications/notification_types/banned_comment.blade.php
@@ -1,7 +1,7 @@
 <div class="card bg-dark border-secondary container-fluid">
     <div class="row">
         <div class="card-body col-8">
-            Your comment was banned <!-- TODO Add references to what post and comment-->
+            Your comment {{ '"' . \Illuminate\Support\Str::limit($notification->bannedComment->text, $limit = 10, $end = '...') . '"' }} was banned <!-- TODO Add references to what post and comment-->
         </div>
     </div>
 </div>
