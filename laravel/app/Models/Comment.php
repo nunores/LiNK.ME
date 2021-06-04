8497f4052fa2d0e78de8068f4bcee90c460751b0
xavier <up201806134@fe.up.pt>
2021-06-04 10:05:27 +0100

    Fixed banned comment notification

--- laravel/app/Models/Comment.php
+++ laravel/app/Models/Comment.php
@@ -44,7 +44,7 @@ class Comment extends Model
      */
     public function post()
     {
-        return $this->belongsTo(Post::class);
+        return $this->belongsTo(Post::class, 'post_id');
     }
 
     /**
